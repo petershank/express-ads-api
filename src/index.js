1f9d85220d0e750e5e4ba4a19aa4d4ccf8478c9d
Peter Shank <peter@shank.com>
2021-10-12 18:12:23 +0000

    change port from 3001 to 80

--- src/index.js
+++ src/index.js
@@ -1,19 +1,17 @@
-'use strict'
+"use strict";
 
 // importing the dependencies
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const helmet = require('helmet');
-const morgan = require('morgan');
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const morgan = require("morgan");
 
 // defining the Express app
 const app = express();
 
 // defining an array to work as the database (temporary solution)
-const ads = [
-  {title: 'Hello, world (again)!'}
-];
+const ads = [{ title: "Hello, world (again)!" }];
 
 // adding Helmet to enhance your API's security
 app.use(helmet());
@@ -25,14 +23,14 @@ app.use(bodyParser.json());
 app.use(cors());
 
 // adding morgan to log HTTP requests
-app.use(morgan('combined'));
+app.use(morgan("combined"));
 
 // defining an endpoint to return all ads
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.send(ads);
 });
 
 // starting the server
-app.listen(3001, () => {
-  console.log('listening on port 3001 like a boss');
+app.listen(80, () => {
+  console.log("listening on port 3001 like a boss");
 });
